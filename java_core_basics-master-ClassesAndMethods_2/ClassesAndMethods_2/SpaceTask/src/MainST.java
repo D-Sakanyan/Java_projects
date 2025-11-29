636fbed7d57f1edca31ce1b7880678b077d21d27
D-Sakanyan <davidsakanyan2003@gmail.com>
2025-11-29 12:23:46 +0400

    Delete java_core_basics-master-ClassesAndMethods_2/ClassesAndMethods_2 directory

--- java_core_basics-master-ClassesAndMethods_2/ClassesAndMethods_2/SpaceTask/src/MainST.java
+++
@@ -1,7 +0,0 @@
-public class MainST {
-    public static void main(String[] args) {
-        System.out.println(MathUtils.pi);
-        System.out.println("S = " + MathUtils.calculateSurfaceArea(6371));
-        System.out.println("C = " + MathUtils.calculateEquatorLength(6371));
-    }
-}
